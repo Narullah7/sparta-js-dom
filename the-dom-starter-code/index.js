eadcf2ca8c860cd2b4462b1aaf801882408384cc
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-01 15:53:48 +0000

    I tried my best to find the solution

---
+++ the-dom-starter-code/index.js
@@ -0,0 +1,18 @@
+var listItems = ["Go out","Come back" , "Buy food" , "Eat food"];
+
+
+function addListItem(value) {
+// write append loop here
+var lis = document.createElement("li");
+for (listItems) {
+
+}
+
+// write the class loop here
+
+
+
+
+
+}
+console.log(document.body);
